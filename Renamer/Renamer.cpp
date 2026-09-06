c85d4a22c5cd55cea9624d8f66fc76c92ec9e617
Narezzurri <110226856+Narezzurri@users.noreply.github.com>
2026-09-06 22:40:01 +0800

    Pack up lib as a .dll

--- Renamer/Renamer.cpp
+++ Renamer/Renamer.cpp
@@ -23,22 +23,22 @@ int main (int argc, const char* argv[])
 		return 0;
 	}
 	INIReader rule (argv[1]);
-// 	if (rule.ParseError() < 0)
-// 	{
-// 		cerr << "Unable to load config file: " << argv[1] << endl;
-// 		cerr << "Error message: " << rule.ParseErrorMessage() << endl;
-// #ifndef		DEBUG
-// 		getchar ();
-// #endif
-// 		return 1;
-// 	}
-// 	vector<string> vec (argc);
-// 	for (int i = 2; i < argc; i++)
-// 		vec[i] = argv[i];
-// 	vector<string> keys = rule.Keys("Replace");
-// 	for (auto pattern : keys)
-// 	{
-// 		string target = rule.Get("Replace", pattern, "");
-// 	}
+	if (rule.ParseError() < 0)
+	{
+		cerr << "Unable to load config file: " << argv[1] << endl;
+		cerr << "Error message: " << rule.ParseErrorMessage() << endl;
+#ifndef		DEBUG
+		getchar ();
+#endif
+		return 1;
+	}
+	vector<string> vec (argc);
+	for (int i = 2; i < argc; i++)
+		vec[i] = argv[i];
+	vector<string> keys = rule.Keys("Replace");
+	for (auto pattern : keys)
+	{
+		string target = rule.Get("Replace", pattern, "");
+	}
 	return 0;
 }
